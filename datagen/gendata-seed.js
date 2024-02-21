8978e957236e7ae773d88ccc3e054b56016bf6d8
Edward Mallia <edmallia@gmail.com>
2024-02-21 10:07:12 +0400

    clean up

--- datagen/gendata-seed.js
+++ datagen/gendata-seed.js
@@ -11,7 +11,7 @@ const noOfUsers = 1000;
 const noOfRoles = 389; //389 is the largest possible number, imposed by the underlying chance.js
 const noOfBuckets = 947; //947 is the largest possible number, imposed by the underlying chance.js
 
-let result = {
+const result = {
     "users" : [],
     "roles" : [],
     "permissions" : ["READ", "WRITE", "DELETE", "MANAGE"],
@@ -19,12 +19,13 @@ let result = {
     "payloads" : ["FINANCIAL", "PII", "CREDENTIALS"]
 }
 
+const outputFile = 'datagen/seed.json';
 
 result.users = chance.unique(chance.name, noOfUsers);
 result.roles = chance.unique(chance.profession, noOfRoles)
 result.buckets = chance.unique(chance.company, noOfBuckets)
 
-fs.writeFile('datagen/seed.json', JSON.stringify(result), err => {
+fs.writeFile(outputFile, JSON.stringify(result), err => {
   if (err) {
     console.error(err);
   } else {
